c48d4804baa0c3984b6f662d7302aba42d4957ad
Akash Maity <am.akashmaity@gmail.com>
2025-10-30 16:37:46 +0530

    completed insert function and only delete from beginning

--- src/double_linked_list.c
+++ src/double_linked_list.c
@@ -51,8 +51,90 @@ int countNodes(NODE *dll) {
     return 1+countNodes(dll->next);
 }
 
+// insert functions
+
+// insert at beginning
+void insertAtBeginning(NODE **dll, int val) {
+    NODE *new = (NODE*)malloc(sizeof(NODE));
+    if(!new) { printf("\nMEMORY ALLOCATION FAILED\n"); return; }
+    new->val = val; new->prev = NULL; new->next = *dll;
+    *dll = new;
+    printf("\nElement inserted at the beginning\n");
+}
+
+// insert at end
+void insertAtEnd(NODE **dll, int val) {
+    NODE *new = (NODE*)malloc(sizeof(NODE));
+    if(!new) { printf("\nMEMORY ALLOCATION FAILED\n"); return; }
+    new->val = val; new->next = NULL;
+    if(!*dll) { new->prev = NULL; *dll = new; }
+    NODE *curr = *dll;
+    while(curr->next) curr=curr->next;
+    curr->next = new;
+    new->prev = curr;
+    printf("Element inserted at the end\n");
+}
+
+// insert at i-th position
+void insertAtPos(NODE **dll, int val, int pos) {
+    if(!*dll) { printf("\nDLL doesn't exist.\n"); return; }
+    NODE *new = (NODE*)malloc(sizeof(NODE));
+    new->val = val;
+    if(pos == 1) return insertAtBeginning(dll, val);
+    int i=1; NODE *curr = *dll;
+    while(curr->next) {
+        if(pos == i) break;
+        curr = curr->next;
+        i++;
+    }
+    if(pos == i+1) { free(new); return insertAtEnd(dll, val); }
+    else if(pos != i) { printf("\nPosition out of bounds\n"); free(new); return; }
+    new->next = curr;
+    new->prev = curr->prev;
+    curr->prev->next = new;
+    curr->prev = new;
+    printf("\nElement inserted at position %d\n", pos);
+}
+
+// insert after an element
+void insertAfterEle(NODE *dll, int val, int key) {
+    if(!dll) { printf("\nDLL doesn't exist\n"); return; }
+    NODE *new = (NODE*)malloc(sizeof(NODE));
+    if(!new) { printf("\nMEMORY ALLOCATION FAILED\n"); return; }
+    new->val = val;
+    while(dll) {
+        if(dll->val == key) break;
+        dll = dll->next;
+    }
+    if(!dll) { printf("\nElement not found\n"); return; }
+    new->next = dll->next;
+    new->prev = dll;
+    if(dll->next) dll->next->prev = new;
+    dll->next = new;
+    printf("\nElement inserted after element %d\n", key);
+}
+
+// delete functions
+
+// delete from beginning
+int deleteFromBeginning(NODE **dll) {
+    if(!*dll) { printf("\nEmpty DLL\n"); return 0; }\
+    NODE *curr = *dll; int val;
+    if(!curr->next) {
+        *dll = NULL;
+        val = curr->val;
+        free(curr);
+        return val;
+    }
+    curr->next->prev = NULL;
+    *dll = curr->next;
+    val = curr->val;
+    free(curr);
+    return val;
+}
+
 void main() {
-    NODE *dll1, *dll2;
+    NODE *dll1 = NULL, *dll2 = NULL;
     while (1)
     {
         int choice;
@@ -72,7 +154,7 @@ void main() {
         printf("\n\n0\tEXIT\n\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
-        int dllchoice;
+        int dllchoice, val, key;
         switch (choice)
         {
         case 1:
@@ -147,9 +229,75 @@ void main() {
             break;
         case 5:
             // insert element in a double linked list
+            printf("Select DLL:\n\n1\tDLL1\n2\tDLL2\n>\t");
+            scanf("%d", &dllchoice);
+            int insertchoice;
+            printf("Select where to insert:\n\n\t1\tbeginning\n\t2\tend\n\t3\ti-th position\n\t4\tafter an element\n\n>\t");
+            scanf("%d", &insertchoice);
+            printf("\nEnter element to insert: ");
+            scanf("%d", &val);
+            switch (insertchoice)
+            {
+            case 1:
+                // insert element at beginning of the dll
+                if(dllchoice == 1) insertAtBeginning(&dll1, val);
+                else if(dllchoice == 2) insertAtBeginning(&dll2, val);
+                else printf("\nInvalid DLL\n");
+                break;
+            case 2:
+                // insert element at the end of the dll
+                if(dllchoice == 1) insertAtEnd(&dll1, val);
+                else if(dllchoice == 2) insertAtEnd(&dll2, val);
+                else printf("\nInvalid DLL\n");
+                break;
+                break;
+            case 3:
+                // insert element at i-th position in the dll
+                printf("\nEnter position (starts from 1): "); scanf("%d", &key);
+                if(dllchoice == 1) insertAtPos(&dll1, val, key);
+                else if(dllchoice == 2) insertAtPos(&dll2, val, key);
+                else printf("\nInvalid DLL\n");
+                break;
+            case 4:
+                // insert elemenet after an element in the dll
+                printf("\nEnter element after which to insert: "); scanf("%d", &key);
+                if(dllchoice == 1) insertAfterEle(dll1, val, key);
+                else if(dllchoice == 2) insertAfterEle(dll2, val, key);
+                else printf("\nInvalid DLL\n");
+                break;
+            default:
+                printf("\nInvalid choice\n");
+                break;
+            }
             break;
         case 6:
             // delete element from a double linked list
+            printf("Select DLL:\n\n1\tDLL1\n2\tDLL2\n>\t");
+            scanf("%d", &dllchoice);
+            int deletechoice;
+            printf("Select where to delete:\n\n\t1\tbeginning\n\t2\tend\n\t3\ti-th position\n\t4\tafter an element\n\n>\t");
+            scanf("%d", &deletechoice);
+            switch (deletechoice)
+            {
+            case 1:
+                // delete from beginning
+                if(dllchoice == 1) printf("\nDeleted from DLL1: %d\n", deleteFromBeginning(&dll1));
+                else if(dllchoice == 2) printf("\nDeleted from DLL2: %d\n", deleteFromBeginning(&dll2));
+                else printf("\nInvalid DLL\n");
+                break;
+            case 2:
+                // delete from end
+                break;
+            case 3:
+                // delete from i-th position
+                break;
+            case 4:
+                // delete after an element
+                break;
+            default:
+                printf("\nInvalid choice\n");
+                break;
+            }
             break;
         case 7:
             // reverse a double linked list
